5d9144ef8072d25caf15f8411c96e5d0209d85e9
fawezferjaoui <fawez1900@gmail.com>
2021-05-24 01:00:13 +0200

    f commit

---
+++ Desktop/rp-starter-master/main.js
@@ -0,0 +1,7 @@
+
+function convertHeight (feet,inches){
+	return ("feet" * 12 + "inches") * 2.54
+
+}
+convertHeight (5,10);
+
---
+++ Desktop/rp1/main.js
@@ -0,0 +1,15 @@
+function square(x) {
+  return x * x;
+}
+function convertHeight(feet, inches){
+  return ("feet" * 12) + "inches") * 2.54
+}
+
+ convertHeight 5;10  ;
+
+function abs(x){
+  if (x >=0){
+    return (x);
+  }
+    return (-x)
+}
